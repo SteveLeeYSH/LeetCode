c80a837a91775a3d6cf1381296b8855f8b5b731d
SteveLeeYSH <60216922+SteveLeeYSH@users.noreply.github.com>
2020-02-10 09:56:49 +0800

    Update 0002.c

--- 0002/0002.c
+++ 0002/0002.c
@@ -13,54 +13,46 @@
  * };
  */
 
-typedef struct ListNode LNode;
-
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
-    LNode *head = (LNode*)malloc(sizeof(LNode));
-    LNode *current = head;
+    struct ListNode *head = l1;
     
-    current->val = l1->val + l2->val;
-    int extra = current->val / 10;
-    current->val %= 10;
+    l1->val += l2->val;
+    int extra = l1->val / 10;
+    l1->val %= 10;
     
     while(l1->next != NULL && l2->next != NULL) {
         l1 = l1->next;
         l2 = l2->next;
-        current->next = (LNode*)malloc(sizeof(LNode));
-        current = current->next;
-        current->val = l1->val + l2->val + extra;
-        extra = current->val / 10;
-        current->val %= 10;
+        l1->val += (l2->val + extra);
+        extra = l1->val / 10;
+        l1->val %= 10;
     }
-    current->next = NULL;
     
     if(l1->next != NULL) {
-        current->next = l1->next;
-        l1->next = NULL;
-        while(extra && current->next != NULL) {
-            current = current->next;
-            current->val += 1;
-            extra = current->val / 10;
-            current->val %= 10;
+        while(extra && l1->next != NULL) {
+            l1 = l1->next;
+            l1->val += 1;
+            extra = l1->val / 10;
+            l1->val %= 10;
         }
     }
-
+    
     if(l2->next != NULL) {
-        current->next = l2->next;
+        l1->next = l2->next;
         l2->next = NULL;
-        while(extra && current->next != NULL) {
-            current = current->next;
-            current->val += 1;
-            extra = current->val / 10;
-            current->val %= 10;
+        while(extra && l1->next != NULL) {
+            l1 = l1->next;
+            l1->val += 1;
+            extra = l1->val / 10;
+            l1->val %= 10;
         }
     }
-
+    
     if(extra == 1) {
-        current->next = (LNode*)malloc(sizeof(LNode));
-        current = current->next;
-        current->val = 1;
-        current->next = NULL;
+        l1->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+        l1 = l1->next;
+        l1->val = 1;
+        l1->next = NULL;
     }
     
     return head;
