b2e6eb14123c1e42649672539e45aa376cc7d25b
SteveLeeYSH <60216922+SteveLeeYSH@users.noreply.github.com>
2020-02-10 10:22:43 +0800

    Update 0002.c

--- 0002/0002.c
+++ 0002/0002.c
@@ -17,30 +17,41 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
     struct ListNode *head = l1;
     
     l1->val += l2->val;
-    int extra = l1->val / 10;
-    l1->val %= 10;
-    
-    while(l1->next != NULL && l2->next != NULL) {
-        l1 = l1->next;
-        l2 = l2->next;
-        l1->val += (l2->val + extra);
-        extra = l1->val / 10;
-        l1->val %= 10;
+    int extra = 0;
+    if(l1->val >= 10) {
+        extra = 1;
+        l1->val -= 10;
     }
     
-    if(l2->next != NULL) {
+    while(l1->next && l2->next) {
+        l1 = l1->next;
+        l2 = l2->next;
+        l1->val = l1->val + l2->val + extra;
+        if(l1->val >= 10) {
+            extra = 1;
+            l1->val -= 10;
+        } else {
+            extra = 0;
+        }
+    }
+    
+    if(l2->next) {
         l1->next = l2->next;
         l2->next = NULL;
     }
     
-    while(extra && l1->next != NULL) {
+    while(extra && l1->next) {
         l1 = l1->next;
         l1->val += 1;
-        extra = l1->val / 10;
-        l1->val %= 10;
+        if(l1->val >= 10) {
+            extra = 1;
+            l1->val -= 10;
+        } else {
+            extra = 0;
+        }
     }
     
-    if(extra == 1) {
+    if(extra) {
         l1->next = (struct ListNode*)malloc(sizeof(struct ListNode));
         l1 = l1->next;
         l1->val = 1;
